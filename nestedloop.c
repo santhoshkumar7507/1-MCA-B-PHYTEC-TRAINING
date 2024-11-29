e5b2bd0380ba383840694a66e72fe7d2c6ae5174
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2024-11-29 15:29:24 +0530

    Add files via upload

---
+++ nestedloop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	printf("enter the numbers");
+	scanf("%d %d",&i,&j);
+	for(i=0;i<=5;++i){
+		printf("%d\n", i);
+	}
+	for(j=0;j<=4;++j){
+		printf("%d\n", j);
+	}
+	return 0;
+}
